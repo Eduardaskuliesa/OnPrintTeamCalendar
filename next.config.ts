d214097bc28b74cb00e81262bb6cef7a263e2013
Eduardaskuliesa <eduardas2000@mail.ru>
2024-12-03 10:41:15 +0200

    feat: NextConfig.ts

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,12 @@ const nextConfig: NextConfig = {
   env: {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET || process.env.AUTH_SECRET,
+    DYNAMODB_NAME: process.env.DYNAMODB_NAME,
+    REGION: process.env.REGION,
+    ACCESS_KEY_ID: process.env.ACCESS_KEY_ID,
+    SECRET_ACCESS_KEY: process.env.SECRET_ACCESS_KEY,
+    VACATION_DYNAMODB_TABLE_NAME: process.env.VACATION_DYNAMODB_TABLE_NAME,
+    SALT_ROUNDS: process.env.SALT_ROUNDS
   }
 };
 
