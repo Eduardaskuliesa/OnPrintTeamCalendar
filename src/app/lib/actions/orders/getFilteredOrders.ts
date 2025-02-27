72ba84f9008bfd882fb8ee90cc6a757e5b7448f5
Eduardaskuliesa <kuliesaeduardas@gmail.com>
2025-02-27 09:06:05 +0200

    chore: limit

--- src/app/lib/actions/orders/getFilteredOrders.ts
+++ src/app/lib/actions/orders/getFilteredOrders.ts
@@ -5,7 +5,7 @@ import { getServerSession } from "next-auth";
 
 async function fetchOrdersFromDb(filters: FilterState, page: number = 1) {
   console.log("Fetching orders from DB at:", new Date().toISOString());
-  const url = new URL(`${process.env.VPS_QUEUE_ENDPOINT}/api/order/filter?page=${page}`);
+  const url = new URL(`${process.env.VPS_QUEUE_ENDPOINT}/api/order/filter?page=${page}&limit=100`);
 
   console.log(filters);
 
