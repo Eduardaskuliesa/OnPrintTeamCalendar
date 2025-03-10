a5505a6236cd03def146615c383435a8c4d67e91
Eduardaskuliesa <kuliesaeduardas@gmail.com>
2025-03-10 12:01:39 +0200

    feat: Fixed bug with draggable element where it started to flash

--- src/app/(main)/email/DragableComponent.tsx
+++ src/app/(main)/email/DragableComponent.tsx
@@ -37,12 +37,24 @@ const DraggableComponent: React.FC<DraggableComponentProps> = ({
 
   const [{ isOver }, drop] = useDrop({
     accept: COMPONENT_TYPE,
-    hover: (item: any) => {
+    hover: (item: any, monitor) => {
       const dragIndex = item.index;
       const hoverIndex = index;
+
       if (dragIndex === hoverIndex) return;
+
       const hoverBoundingRect = ref.current?.getBoundingClientRect();
       if (!hoverBoundingRect) return;
+
+      const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+      const clientOffset = monitor.getClientOffset();
+      if (!clientOffset) return;
+
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) return;
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) return;
+
       moveComponent(dragIndex, hoverIndex);
       item.index = hoverIndex;
     },
@@ -58,7 +70,7 @@ const DraggableComponent: React.FC<DraggableComponentProps> = ({
       case "button":
         return <Button {...component.props} />;
       case "image":
-        return <EmailImage {...component.props} />
+        return <EmailImage {...component.props} />;
       default:
         return <div>Unknown component type</div>;
     }
